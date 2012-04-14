44b6586ad2878dc4f69e3e699c0189f580d5e2c1
Michael Farrell <micolous+git@gmail.com>
2012-04-15 00:46:29 +0930

    Pulled this out of one of my other projects into it's own standalone library.  Now licensed under LGPL3 (instead of GPL3).

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name="configparser_plus",
+	version="1.0",
+	description="Library to provide a better version of ConfigParser in Python 2 and 3.",
+	author="Michael Farrell",
+	author_email="micolous@gmail.com",
+	url="https://github.com/micolous/configparser_plus",
+	
+	py_modules=['configparser_plus'],
+)
+
